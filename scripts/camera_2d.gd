9a2a2b4c4ae96cd57366813a92cec0e3537ca2b7
Anemois <danieltang1668@gmail.com>
2026-05-09 23:53:01 +0800

    camera fix and dummy node fix

---
+++ scripts/camera_2d.gd
@@ -0,0 +1,23 @@
+extends Camera2D
+
+var zoom_pos = Vector2(576, 324)
+
+var max_zoom_movement = Vector2(576, 324)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		# zoom in
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			zoom_pos = get_global_mouse_position()
+			self.zoom += Vector2(0.1, 0.1) * self.zoom
+			self.position += (zoom_pos - self.position)/(max_zoom_movement) * 50
+		# zoom out
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			zoom_pos = get_global_mouse_position()
+			self.zoom -= Vector2(0.1, 0.1) * self.zoom
+			self.position += (self.position - zoom_pos)/max_zoom_movement * 50
+			# call the zoom function
+
+
+func _on_pop_button_pressed() -> void:
+	pass # Replace with function body.
